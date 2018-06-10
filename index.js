cafd26d2a8ae4822ac47c55b55495444b6ae5b14
AbrarKL <abrarlone01@outlook.com>
2018-06-10 15:22:40 +0100

    forgot to remove module

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ function getCookie() {
 		timeout: 10000,
 		jar: jar,
 	});
-	const fs = require('fs');
 	const Generator = require('./Generator');
 	const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36';
 
