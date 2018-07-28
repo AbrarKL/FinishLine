4511c1f6c60865b90b0d67e1338328038ede9e4a
AbrarKL <abrarlone01@outlook.com>
2018-07-28 22:58:43 +0100

    updated main file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var productURL = 'https://www.finishline.com/store/product/mens-adidas-ultraboost-clima-x-parley-running-shoes/prod2778311?styleId=BB7076&colorId=WTB';
+var ckie = "";
 getCookie();
 
 function getCookie() {
@@ -10,7 +11,7 @@ function getCookie() {
 	const Generator = require('./Generator');
 	const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36';
 	const cheerio = require('cheerio');
-	
+
 	request.get({
 			headers: {
 				'cache-control': 'max-age=0',
@@ -28,61 +29,61 @@ function getCookie() {
 			for (var i = 0; i < cookies.length; i++) {
 				if (cookies[i].key == '_abck') {
 					console.log("Found Initial _abck Cookie: " + cookies[i].value);
+					ckie = cookies[i].value;
 				}
 			}
 			request({
-				url: 'https://www.finishline.com/_bm/_data',
-				method: 'POST',
-				json: true,
-				body: {
-					"sensor_data": Generator.generateSensorData('https://www.finishline.com/')
+					url: 'https://www.finishline.com/_bm/_data',
+					method: 'POST',
+					json: true,
+					body: {
+						"sensor_data": Generator.generateSensorData('https://www.finishline.com/', ckie)
+					},
+					headers: {
+						'Origin': 'https://www.finishline.com',
+						'Referer': 'https://www.finishline.com/',
+						'user-agent': userAgent
+					}
 				},
-				headers: {
-					'Origin': 'https://www.finishline.com',
-					'Referer': 'https://www.finishline.com/',
-					'user-agent': userAgent
-				}
-			}, 
-			function (error, response, body) {
-				const parsed = JSON.parse(JSON.stringify(jar));
-				const cookies = parsed["_jar"]["cookies"];
-				for (var i = 0; i < cookies.length; i++) {
-					if (cookies[i].key == '_abck') {
-						const _abck = cookies[i].value;
-						if (_abck.indexOf('~0~') > -1) {
-							console.log("Found Valid _abck Cookie: " + cookies[i].value);
-							request.get({
-								headers: {
-									'user-agent': userAgent
-								},
-								url: productURL
-							},
-							function (error, response, body) {
-								if(response.statusCode == 200)
-								{
-									$ = cheerio.load(body);
-									var itemName = $('.row #title[itemprop="name"]').html().trimLeft().trimRight().replace('&apos;', "'");
-									console.log("Found Product Page")
-									console.log(itemName);
-									console.log("Sizes Available: ")
-									var sizes = $('#productSizes .column .button:not(.disabled)');
-									for(var i = 0; i < sizes.length; i++)
-									{
-										var sizeInfo = $('#productSizes .column .button:not(.disabled)')[i]["attribs"];
-										console.log("Size: " + sizeInfo["aria-label"] + " SKU:" + sizeInfo["data-sku"])
+				function (error, response, body) {
+					const parsed = JSON.parse(JSON.stringify(jar));
+					const cookies = parsed["_jar"]["cookies"];
+					for (var i = 0; i < cookies.length; i++) {
+						if (cookies[i].key == '_abck') {
+							const _abck = cookies[i].value;
+							if (_abck.indexOf('~0~') > -1) {
+								console.log("Found Valid _abck Cookie: " + _abck);
+								ckie = _abck;
+								request.get({
+										headers: {
+											'user-agent': userAgent
+										},
+										url: productURL
+									},
+									function (error, response, body) {
+										if (response.statusCode == 200) {
+											$ = cheerio.load(body);
+											var itemName = $('.row #title[itemprop="name"]').html().trimLeft().trimRight().replace('&apos;', "'");
+											console.log("Found Product Page")
+											console.log(itemName);
+											console.log("Sizes Available: ")
+											var sizes = $('#productSizes .column .button:not(.disabled)');
+											for (var i = 0; i < sizes.length; i++) {
+												var sizeInfo = $('#productSizes .column .button:not(.disabled)')[i]["attribs"];
+												console.log("Size: " + sizeInfo["aria-label"] + " SKU:" + sizeInfo["data-sku"])
+											}
+										}
 									}
-								}
+								);
+							} else {
+								console.log("Cookie not valid. Trying again in 2 seconds.")
+								setTimeout(function () {
+									getCookie();
+								}, 2000);
 							}
-						);
-						} else {
-							console.log("Cookie not valid. Trying again in 2 seconds.")
-							setTimeout(function () {
-								getCookie();
-							}, 2000);
 						}
 					}
-				}
-			});
+				});
 		}
 	);
 }
